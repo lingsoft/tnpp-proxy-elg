d1a5f2e7866e44c331a6ef5e924e1b887332e7b6
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-07-08 10:36:30 +0300

    Improve error handling

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ endpoint = "http://localhost:8001/process"
 class TnppProxy(FlaskService):
 
     def process_text(self, request: TextRequest):
-        headers = {'Content-Type': 'application/json'}
-        payload = {
-                "type": "text",
-                "content": request.content,
-                "params": request.params}
-        payload = json.dumps(payload)
         try:
+            headers = {'Content-Type': 'application/json'}
+            payload = {
+                    "type": "text",
+                    "content": request.content,
+                    "params": request.params}
+            payload = json.dumps(payload)
             response = requests.post(
                     endpoint, headers=headers, data=payload)
         except Exception:
@@ -25,14 +25,19 @@ class TnppProxy(FlaskService):
                     params=["Error calling remote API"])
             return Failure(errors=[error])
         if response.ok:
-            data = response.json()
-            if "response" in data:
-                return AnnotationsResponse(
-                        annotations=data["response"]["annotations"])
-            if "failure" in data:
-                return Failure(errors=data["failure"]["errors"])
+            try:
+                data = response.json()
+                if "response" in data:
+                    return AnnotationsResponse(
+                            annotations=data["response"]["annotations"])
+                if "failure" in data:
+                    return Failure(errors=data["failure"]["errors"])
+            except Exception:
+                error = StandardMessages.generate_elg_service_internalerror(
+                        params=["Invalid response from remote API"])
+                return Failure(errors=[error])
             error = StandardMessages.generate_elg_service_internalerror(
-                    params=["Invalid response from remote API"])
+                    params=["No response or failure from remote API"])
             return Failure(errors=[error])
         else:
             error = StandardMessages.generate_elg_service_internalerror(
